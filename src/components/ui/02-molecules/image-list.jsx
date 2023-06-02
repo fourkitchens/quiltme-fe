16b28f43fae7d7bca3279d6118e83bfd555e05c1
Roberto Hernandez <roberthb94@gmail.com>
2023-06-02 08:39:55 -0600

    feat(quiltme-149): add images grid component

---
+++ src/components/ui/02-molecules/image-list.jsx
@@ -0,0 +1,246 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import LightGallery from "lightgallery/react";
+
+// Lightgallery.
+import "lightgallery/css/lightgallery.css";
+import "../../../styles/lg-overrides.css";
+
+export default function ImageList({ onInit, setIndex }) {
+  const wrapperStyles = {
+    ".lg-react-element": {
+      m: "-1vw",
+      flexWrap: "wrap",
+      overflow: "hidden",
+      display: "inline-flex",
+      transformOrigin: "left top",
+      backfaceVisibility: "hidden",
+      width: { xs: "30rem", lg: "140rem" },
+      transform: "scale(1.2) rotate(-1deg) translate(-1%)",
+    },
+  };
+
+  const linkStyles = {
+    cursor: "pointer",
+    overflow: "hidden",
+    position: "relative",
+    aspectRatio: "1 / 1",
+    display: "inline-flex",
+    width: { xs: "10rem",  lg: "20rem" },
+    "&:before": {
+      content: "''",
+      opacity: 0.3,
+      width: "100%",
+      height: "100%",
+      position: "absolute",
+      transition: "all 0.5s",
+      bgcolor: "nice-black.main",
+    },
+    "&.active:before": {
+      opacity: 0.5,
+      bgcolor: "sunset-orange.main",
+    },
+  };
+
+  const imageStyles = {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  };
+
+  const handleClick = (event) => {
+    const selected = event.currentTarget.getAttribute("data-key");
+    const index = parseInt(selected, 10);
+    setIndex(index);
+  };
+
+  return (
+    <Stack sx={wrapperStyles}>
+      <LightGallery onInit={onInit} counter={false} download={false}>
+        {itemData.map((item, index) => (
+          <Link
+            key={index}
+            sx={linkStyles}
+            data-key={index}
+            data-src={item.img}
+            onClick={handleClick}
+          >
+            <Box
+              loading="lazy"
+              component="img"
+              alt={item.title}
+              sx={imageStyles}
+              src={`${item.img}?w=800&fit=cover&auto=format`}
+              srcSet={`${item.img}?w=800&fit=cover&auto=format&dpr=2 2x`}
+            />
+          </Link>
+        ))}
+      </LightGallery>
+    </Stack>
+  );
+}
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  // {
+  //   img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+  //   title: "Coffee",
+  // },
+];
--- src/components/wrappers/page-layout-wrapper.jsx
+++ src/components/wrappers/page-layout-wrapper.jsx
@@ -1,14 +1,30 @@
 import PropTypes from 'prop-types';
-import PageHeader from '../ui/02-molecules/page-header';
-import PageFooter from '../ui/02-molecules/page-footer';
+import { Box, Container } from '@mui/material';
+import PageHeader from '../ui/03-organisms/page-header';
+import PageFooter from '../ui/03-organisms/page-footer';
 
 export default function PageLayoutWrapper({ children }) {
+  const containerStyles = {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    overflow: 'hidden',
+    flexFlow: 'column nowrap',
+  };
+
+  const bodyStyles = {
+    flex: 1,
+    overflow: 'auto',
+  };
+  
   return (
-    <div>
+    <Container sx={containerStyles} maxWidth={false} disableGutters>
       <PageHeader />
-      {children}
+        <Box sx={bodyStyles}>
+          {children}
+        </Box>
       <PageFooter />
-    </div>
+    </Container>
   );
 }
 
